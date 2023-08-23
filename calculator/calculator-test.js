086182b117368a95bf9ddb79ba6e53c5e3f6daa2
thisisfoobar <matt.mcelreath26@gmail.com>
2023-08-23 19:42:42 -0400

    Update Calculator.js and create several unit tests.  Fix bug with title of .html file

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,27 @@
-
-it('should calculate the monthly rate correctly', function () {
-  // ...
+it("should calculate the monthly rate correctly", function () {
+  expect(calculateMonthlyPayment({ amount: 100000, years: 10, rate: 6 })).toBe(
+    "1110.21"
+  );
+  expect(calculateMonthlyPayment({ amount: 100000, years: 10, rate: 'a' })).toBe('NaN');
 });
 
-
-it("should return a result with 2 decimal places", function() {
-  // ..
+it("should return a result with 2 decimal places", function () {
+  expect(calculateMonthlyPayment({ amount: 10, years: 10, rate: 6 })).toBe(
+    "0.11"
+  );
 });
 
-/// etc
+it("should return a string value", function () {
+  expect(
+    calculateMonthlyPayment({ amount: 100000, years: 10, rate: 6 })
+  ).toBeInstanceOf(String);
+});
+
+// Can you only run a test on a function that takes in values?
+// it("should set initial values", function () {
+//   expect(setupIntialValues()).toBe();
+// });
+
+// it("should return proper values from UI", function () {
+//   expect(getCurrentUIValues()).toBe({amount: 100000, years: 10, rate: 6})
+// });
--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,8 +1,9 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("calc-form");
+
   if (form) {
     setupIntialValues();
-    form.addEventListener("submit", function(e) {
+    form.addEventListener("submit", function (e) {
       e.preventDefault();
       update();
     });
@@ -11,30 +12,51 @@ window.addEventListener('DOMContentLoaded', function() {
 
 function getCurrentUIValues() {
   return {
-    amount: +(document.getElementById("loan-amount").value),
-    years: +(document.getElementById("loan-years").value),
-    rate: +(document.getElementById("loan-rate").value),
-  }
+    amount: +document.getElementById("loan-amount").value,
+    years: +document.getElementById("loan-years").value,
+    rate: +document.getElementById("loan-rate").value,
+  };
 }
 
 // Get the inputs from the DOM.
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  const loanAmount = document.getElementById("loan-amount");
+  const loanYears = document.getElementById("loan-years");
+  const loanRate = document.getElementById("loan-rate");
+  loanAmount.value = 100000;
+  loanYears.value = 10;
+  loanRate.value = 6;
+
+  update();
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  updateMonthly(calculateMonthlyPayment(getCurrentUIValues()));
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  let monthlyRate = values.rate / 100 / 12;
+  let months = values.years * 12;
+
+  return (
+    (values.amount * monthlyRate) /
+    (1 - Math.pow(1 + monthlyRate, -1 * months))
+  )
+    .toFixed(2)
+    .toString();
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  const monthlyPayment = document.getElementById("monthly-payment");
+
+  monthlyPayment.innerText = "$" + monthly;
 }
